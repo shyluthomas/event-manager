0aa6566072af15d21f25efa1ccfe09e099fb2475
shyluthomas <shylupthomas@gmail.com>
2024-02-18 07:31:48 +0000

    Step: Cors issue is fixed
    Signed-off-by: shyluthomas <shylupthomas@gmail.com>

--- server.ts
+++ server.ts
@@ -5,6 +5,7 @@ import express from "express";
 import { userRoutes, healthRoute } from "./routes";
 import { logger, errorHandler } from "./middlewares";
 import { authRoutes } from "./routes/authRoutes";
+import cors = require("cors");
 
 export const startServer = () => {
   const port = process.env.PORT || 8044;
@@ -12,6 +13,7 @@ export const startServer = () => {
   app.use(express.json());
 
   app.use(logger);
+  app.use((req, res, next) => { next(); }, cors({maxAge: 84600}));
   app.use("/health", healthRoute);
   app.use("/user", userRoutes);
   app.use("/auth", authRoutes);
